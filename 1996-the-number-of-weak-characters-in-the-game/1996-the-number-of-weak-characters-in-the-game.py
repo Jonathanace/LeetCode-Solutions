a5e9d50e9c38e4eb6c4fd8172842ea9da65d9fb4
Jonathanace <55035716+Jonathanace@users.noreply.github.com>
2022-09-08 19:27:08 -0700

    Time: 2402 ms (89.08%), Space: 67.1 MB (79.12%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -1,11 +1,14 @@
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
-        properties.sort(key=lambda x: (-x[0],x[1])) # sorted by descending attack, ascending defense
-        prev_def = 0
-        res = 0
-        for character in properties: 
-            if character[1] < prev_def:
-                res += 1 
+        
+        properties.sort(key=lambda x: (-x[0],x[1]))
+        
+        ans = 0
+        curr_max = 0
+        
+        for _, d in properties:
+            if d < curr_max:
+                ans += 1
             else:
-                prev_def = character[1]
-        return res
+                curr_max = d
+        return ans
