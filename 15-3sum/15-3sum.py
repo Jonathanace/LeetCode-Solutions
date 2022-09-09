ab77e0032e5cf4fa49a275198dbc626ee9eaef71
Jonathanace <55035716+Jonathanace@users.noreply.github.com>
2022-09-09 14:55:50 -0700

    Time: 633 ms (98.26%), Space: 18.5 MB (15.13%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,46 +1,37 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-
         res = set()
-
-        #1. Split nums into three lists: negative numbers, positive numbers, and zeros
-        n, p, z = [], [], []
-        for num in nums:
+        pos, zero, neg = [], [], []
+        for num in nums: 
             if num > 0:
-                p.append(num)
-            elif num < 0: 
-                n.append(num)
+                pos.append(num)
+            elif num < 0:
+                neg.append(num)
             else:
-                z.append(num)
-
-        #2. Create a separate set for negatives and positives for O(1) look-up times
-        N, P = set(n), set(p)
-
-        #3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
-        #   i.e. (-3, 0, 3) = 0
-        if z:
-            for num in P:
-                if -1*num in N:
-                    res.add((-1*num, 0, num))
-
-        #3. If there are at least 3 zeros in the list then also include (0, 0, 0) = 0
-        if len(z) >= 3:
-            res.add((0,0,0))
-
-        #4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
-        #   exists in the positive number set
-        for i in range(len(n)):
-            for j in range(i+1,len(n)):
-                target = -1*(n[i]+n[j])
-                if target in P:
-                    res.add(tuple(sorted([n[i],n[j],target])))
-
-        #5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
-        #   exists in the negative number set
-        for i in range(len(p)):
-            for j in range(i+1,len(p)):
-                target = -1*(p[i]+p[j])
-                if target in N:
-                    res.add(tuple(sorted([p[i],p[j],target])))
-
+                zero.append(num)
+                
+        pos_set, neg_set = set(pos), set(neg)
+        
+        if zero:
+            for num in pos:
+                if -1 * num in neg_set:
+                    res.add((0, -1 * num, num))
+                
+        if len(zero) >= 3:
+            res.add((0, 0, 0))
+            
+        # negative pairs
+        for i in range(len(neg)):
+            for j in range(i+1, len(neg)):
+                target = -1 * (neg[i] + neg[j])
+                if target in pos_set:
+                    res.add(tuple(sorted([target, neg[i], neg[j]])))
+        
+        # positive pairs
+        for i in range(len(pos)):
+            for j in range(i+1, len(pos)):
+                target = -1 * (pos[i] + pos[j])
+                if target in neg_set:
+                    res.add(tuple(sorted([target, pos[i], pos[j]])))
+        
         return res
