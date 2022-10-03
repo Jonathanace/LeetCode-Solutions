19d00b5821d9a54de0f8fb454e6d6477ad405a2b
Jonathanace <55035716+Jonathanace@users.noreply.github.com>
2022-10-03 13:30:31 -0700

    Time: 3421 ms (5.12%), Space: 24.9 MB (52.90%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        time = 0
+        i = 0
+        while i < len(colors):
+            
+            # find span of same colored baloons
+            j = i + 1
+            while j < len(colors) and colors[j] == colors[i]:
+                j += 1
+            # print(colors[i:j])
+            # delete all baloons except the one that takes the most time to remove
+            time += sum(sorted(neededTime[i:j])[:-1])
+            i = j-1
+            i += 1
+        return time
