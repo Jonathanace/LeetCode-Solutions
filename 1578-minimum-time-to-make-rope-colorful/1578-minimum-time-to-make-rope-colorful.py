b7b686c0f6ccd95d0f4335cb9b65e11cb9e14c66
Jonathanace <55035716+Jonathanace@users.noreply.github.com>
2022-10-03 13:31:07 -0700

    Time: 2131 ms (47.48%), Space: 24.8 MB (74.93%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -3,14 +3,15 @@ class Solution:
         time = 0
         i = 0
         while i < len(colors):
-            
             # find span of same colored baloons
             j = i + 1
             while j < len(colors) and colors[j] == colors[i]:
                 j += 1
-            # print(colors[i:j])
+                
             # delete all baloons except the one that takes the most time to remove
             time += sum(sorted(neededTime[i:j])[:-1])
+            
+            # update i
             i = j-1
             i += 1
         return time
