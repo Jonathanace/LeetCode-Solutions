425500e673510d8763b1cbf601866310c1b18b70
Jonathanace <55035716+Jonathanace@users.noreply.github.com>
2022-10-24 21:38:54 -0700

    Time: 72 ms (84.01%), Space: 18.3 MB (27.55%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,29 +1,23 @@
 class MinStack:
     def __init__(self):
-        self.q = []
+        self.stack = []
 
     def push(self, val):
         minimum = val
         if self.getMin() is not None:
             minimum = min(minimum, self.getMin())
-        self.q.append((val, minimum));
+        self.stack.append((val, minimum));
 
     # @return nothing
     def pop(self):
-        self.q.pop()
+        self.stack.pop()
 
 
     # @return an integer
     def top(self):
-        if len(self.q) == 0:
-            return None
-        else:
-            return self.q[len(self.q) - 1][0]
+        return self.stack[-1][0] if self.stack else None
 
 
     # @return an integer
     def getMin(self):
-        if len(self.q) == 0:
-            return None
-        else:
-            return self.q[len(self.q) - 1][1]
+        return self.stack[-1][1] if self.stack else None
