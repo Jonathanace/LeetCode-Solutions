30fc9682d51446c3258a6b490dbb4159e9a87485
Jonathanace <55035716+Jonathanace@users.noreply.github.com>
2023-01-03 15:10:48 -0800

    Time: 395 ms (39.74%), Space: 14.6 MB (61.11%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        res = 0
+        for i in range(len(strs[0])):
+            l = [j[i] for j in strs]
+            # print(l)
+            # print(sorted(l))
+            if l != sorted(l):
+                res += 1
+        return res
