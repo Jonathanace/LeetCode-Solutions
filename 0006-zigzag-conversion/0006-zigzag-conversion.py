da27211e7cc085f38bfa156c3f9ba0769d6cbf3a
Jonathanace <55035716+Jonathanace@users.noreply.github.com>
2023-02-03 08:51:07 -0800

    Time: 58 ms (79.24%), Space: 14.1 MB (48.57%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,7 +1,6 @@
 class Solution:
     def convert(self, s: str, n: int) -> str:
         res = [[] for _ in range(n)]
-        # print(res)
         i = 0
         l = len(s)
         while i < l:
@@ -9,15 +8,12 @@ class Solution:
             while j < n and (i + j) < l: # doing down
                 res[j].append(s[i + j])
                 j += 1
-                
-            # print(res)
-
             i += j
             j = 0
+            
             while j < n-2 and (i + j) < l: # going up
                 res[-(j+2)].append(s[i+j])
                 j += 1
-            
             i += j
             
         return ''.join([''.join(i) for i in res])
