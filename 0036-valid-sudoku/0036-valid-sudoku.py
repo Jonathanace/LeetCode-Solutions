28fb2972b8addfa45025fae7409aac25505a9407
Jonathanace <55035716+Jonathanace@users.noreply.github.com>
2023-04-19 19:12:42 -0700

    Time: 111 ms (9.98%), Space: 13.8 MB (69.14%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -3,31 +3,29 @@ class Solution:
         squares, rows, columns = defaultdict(set), defaultdict(set), defaultdict(set)
         for row in range(9):
             for column in range(9):
-                num = board[row][column]
-                if num == '.':
+                # check for valid number
+                try:
+                    num = int(board[row][column])
+                except:
                     continue
-                else:
-                    num = int(num)
                 
+                # check row
                 if num in rows[row]:
-                    # print(num, row)
                     return False
                 else:
                     rows[row].add(num)
                     
+                # check column
                 if num in columns[column]:
-                    # print(num, column)
                     return False
                 else:
                     columns[column].add(num)
                     
+                # check square
                 square = (row // 3, column // 3)
                 if num in squares[square]:
-                    # print(num, square)
                     return False
                 else:
                     squares[square].add(num)
-        return True
-#0, 1, 2
-#3, 4, 5
-#6, 7 8
+        
+        return True 
