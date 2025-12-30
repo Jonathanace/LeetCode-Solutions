94461ec2bfa1c6dfba6a3ff82ba019b3da06fe7f
Jonathanace <55035716+Jonathanace@users.noreply.github.com>
2025-12-30 13:24:14 -0800

    Time: 0 ms (100%), Space: 17.3 MB (89.76%) - LeetHub

--- LeetCode/Medium/0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ LeetCode/Medium/0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -9,31 +9,21 @@ class Solution:
         return count
 
     def isMagicSquare(self, grid: List[List[int]], row: int, col: int) -> bool:
+        if grid[row+1][col+1] != 5:
+            return False
+        
         offsets = [(offset_row, offset_col) for offset_row in range(3) for offset_col in range(3)]
         vals = [grid[row+offset_row][col+offset_col] for offset_row, offset_col in offsets]
-        if set(vals) != set(range(1,10)):
-            return False
-        v_1 = vals[0] + vals[3] + vals[6]
-        v_2 = vals[1] + vals[4] + vals[7]
-        if v_1 != v_2:
-            return False
-        v_3 = vals[2] + vals[5] + vals[8]
-        if v_3 != v_2:
-            return False
-        h_1 = vals[0] + vals[1] + vals[2]
-        if h_1 != v_3: 
-            return False
-        h_2 = vals[3] + vals[4] + vals[5]
-        if h_2 != h_1:
-            return False
-        h_3 = vals[6] + vals[7] + vals[8]
-        if h_3 != h_2:
-            return False
-        d_1 = vals[0] + vals[4] + vals[8]
-        if d_1 != h_3:
-            return False
-        d_2 = vals[6] + vals[4] + vals[2]
-        if d_2 != d_1:
+        if sorted(vals) != [1,2,3,4,5,6,7,8,9]:
             return False
 
-        return True
+        v_1 = vals[0] + vals[3] + vals[6]
+        v_2 = vals[1] + vals[4] + vals[7]
+        v_3 = vals[2] + vals[5] + vals[8]
+        h_1 = vals[0] + vals[1] + vals[2]
+        h_2 = vals[3] + vals[4] + vals[5]
+        h_3 = vals[6] + vals[7] + vals[8]
+        d_1 = vals[0] + vals[4] + vals[8]
+        d_2 = vals[6] + vals[4] + vals[2]
+
+        return v_1 == v_2 == v_3 == h_1 == h_2 == h_3 == d_1 == d_2
