16dd48ec249674228c30cb2aaf157b71e76b5088
Jonathanace <55035716+Jonathanace@users.noreply.github.com>
2025-12-30 13:16:33 -0800

    Time: 7 ms (2.41%), Space: 17.3 MB (97.59%) - LeetHub

---
+++ LeetCode/Medium/0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        count = 0
+        rows, cols = len(grid), len(grid[0])
+        for row in range(rows-2):
+            for col in range(cols-2):
+                if self.isMagicSquare(grid, row, col):
+                    count += 1
+        return count
+
+    def isMagicSquare(self, grid: List[List[int]], row: int, col: int) -> bool:
+        offsets = [(offset_row, offset_col) for offset_row in range(3) for offset_col in range(3)]
+        vals = [grid[row+offset_row][col+offset_col] for offset_row, offset_col in offsets]
+        v_1 = vals[0] + vals[3] + vals[6]
+        v_2 = vals[1] + vals[4] + vals[7]
+        v_3 = vals[2] + vals[5] + vals[8]
+        h_1 = vals[0] + vals[1] + vals[2]
+        h_2 = vals[3] + vals[4] + vals[5]
+        h_3 = vals[6] + vals[7] + vals[8]
+        d_1 = vals[0] + vals[4] + vals[8]
+        d_2 = vals[6] + vals[4] + vals[2]
+        if set(vals) != set(range(1,10)):
+            return False
+        return v_1 == v_2 == v_3 == h_1 == h_2 == h_3 == d_1 == d_2
